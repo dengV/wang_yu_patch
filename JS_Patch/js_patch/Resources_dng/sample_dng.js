980a744bc6f4909323b4cc97a0d6fc0d3f0d22e4
邓江洲 <dengjingzhou@zto.cn>
2018-05-24 14:16:20 +0800

    修正 js  sample资源 ， 不加 反斜杠， 好像不行

--- JS_Patch/js_patch/Resources_dng/sample_dng.js
+++ JS_Patch/js_patch/Resources_dng/sample_dng.js
@@ -1,5 +1,5 @@
-var alertView = require('UIAlertView').alloc.init();
-alertView.setTitle('Alert_邓');
-alertView.setMessage('AlertView From JS File');
-alertView.addButtonWithTitle('OK');
-alertView.show();
+var alertView = require('UIAlertView').alloc.init();\
+alertView.setTitle('Alert_邓');\
+alertView.setMessage('AlertView From JS File');\
+alertView.addButtonWithTitle('OK');\
+alertView.show();\
